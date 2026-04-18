ad6ebe8d535c638229d3a7b780584db4584ed6eb
KAPKEPOT <185211085+KAPKEPOT@users.noreply.github.com>
2026-04-18 20:45:48 +0300

    Add files via upload

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,4 @@
 export { Badge, StatusDot } from './Badge';
 export { StatCard } from './StatCard';
 export { SectionTitle } from './SectionTitle';
-export { CodeBlock } from './CodeBlock';
-export { Spinner, Btn, Modal, EmptyState, Alert, Input } from './Shared';
+export { CodeBlock, Spinner, Btn, Modal, EmptyState, Alert, Input } from './Shared';
